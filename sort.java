4669db0c2ec8d8e5a4ba3b342dca350f71ba5045
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-02-06 21:29:52 +0400

    Update sort.java

--- sort.java
+++ sort.java
@@ -1,17 +1,23 @@
-public class sort{
-	public static void main(String[] arg){
-		int [] a={-2, 3, 0, 5, 9};
-		for (int i=0; i<a.length; i++){
-			for (int j=1; j<a.length; j++){
-				if (a[j-1]>a[j]){
-					int min=a[j];
-					a[j]=a[j-1];
-					a[j-1]=min;
-				 }
-			 }
-		}
-		for (int b:a)
-			System.out.println(b);
-	}
+public class sort {
+  public static void main(String[] arg) {
+    int[] a = {
+      -2,
+      3,
+      0,
+      5,
+      9
+    };
+    for (int i = 0; i < a.length; i++) {
+      for (int j = 1; j < a.length; j++) {
+        if (a[j - 1] > a[j]) {
+          int min = a[j];
+          a[j] = a[j - 1];
+          a[j - 1] = min;
+        }
+      }
+    }
+    for (int b: a)
+      System.out.println(b);
+  }
 
 }
